9ff7754a6bdb9d80afb40ccd3af3091b7ee3508a
George <buleandrageorge@gmail.com>
2022-06-07 15:25:18 +0100

    Create view for pdf, add link and pdf

--- myfirstApp.py
+++ myfirstApp.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, send_from_directory
+from flask.helpers import url_for
 from flask_pymongo import PyMongo
+import os
 app = Flask(__name__)
 """
 app.config["MONGO_URI"] = "mongodb+srv://george:tKlOrnl7yoyzwkQw@all.nmpma.mongodb.net/?retryWrites=true&w=majority"
@@ -22,3 +24,8 @@ def developer():
 @app.route("/dreamer")
 def dreamer():
     return render_template("dreamer.html");
+
+@app.route("/cv")
+def view_cv():
+    path = "./static/docs/"
+    return send_from_directory(path, 'cv_pdf.pdf')
