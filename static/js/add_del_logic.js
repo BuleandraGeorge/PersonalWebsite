5b786c02b490d1fd0563aaab56c05745f1e42b1b
George <buleandrageorge@gmail.com>
2022-06-08 18:33:27 +0100

    add inputs for technologies, others. feature to add more inputs

--- static/js/add_del_logic.js
+++ static/js/add_del_logic.js
@@ -1,24 +1,29 @@
-document.getElementById('add').addEventListener('click', function () {
-    newInput = document.createElement('input');
-    newInput.setAttribute('placeholder', 'Add Feature');
-    newInput.setAttribute('type', 'text');
-    newInput.setAttribute('class', 'form-control mb-2');
-    newInput.setAttribute('name', 'no_name');
-    referenceNode = document.getElementById('features_buttons')
-    document.getElementById('features_inputs').insertBefore(newInput, referenceNode);
-})
 
-document.getElementById('del').addEventListener('click', function () {
-    referenceNode = document.getElementById('add')
-    list = document.getElementById('features_inputs').childNodes
-    let inputs = [];
-    for (element in list) {
-        if (list[element].tagName == "INPUT") {
-            inputs.push(list[element]);
-		}
-    }
-    if (inputs.length>1)
+document.addEventListener('click', function (e) {
+    let element = e.target
+    let button = e.target
+    if (element.tagName == "I") button = element.parentNode
+    if (button.className.indexOf('add') != -1)
     {
-        inputs[inputs.length - 1].remove();
+        initialInput = button.parentNode.previousElementSibling;
+        newInput = document.createElement('input');
+        newInput.setAttribute('placeholder', initialInput.placeholder);
+        newInput.setAttribute('type', initialInput.type);
+        newInput.setAttribute('class', initialInput.className);
+        newInput.setAttribute('name', initialInput.name);
+        initialInput.parentNode.insertBefore(newInput, button.parentNode);
     }
+    else if (button.className.indexOf('del') != -1)
+    {
+        let input_container = button.parentNode.parentNode.childNodes
+        let inputs =[]
+        for (child in input_container) {
+            if(input_container[child].tagName=="INPUT") inputs.push(input_container[child])
+		}
+        if (inputs.length > 1) {
+            inputs[inputs.length-1].remove();
+		}
+       
+	}
+
 })
