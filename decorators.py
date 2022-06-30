49117e9c7ba8229264d02a0375bd8dca5ea9b022
George <buleandrageorge@gmail.com>
2022-06-30 20:50:57 +0100

    Owner views available only in development

--- decorators.py
+++ decorators.py
@@ -1,11 +1,12 @@
 from functools import wraps
 from flask import request, redirect, url_for, session
+import os
 
 def isOwner(database):
 	def decorator(f):
 		@wraps(f)
 		def decorated_function(*args,**kwargs):
-			if not 'user_uuid' in  session.keys() or  database.owner.find_one({'user_uuid':session.get('user_uuid', "xxxxxxxx")}) is None:
+			if os.environ["FLASK_ENV"]!="development" or not 'user_uuid' in  session.keys() or  database.owner.find_one({'user_uuid':session.get('user_uuid', "xxxxxxxx")}) is None:
 				return redirect(url_for('login'))
 			return f(*args, **kwargs)
 		return decorated_function
